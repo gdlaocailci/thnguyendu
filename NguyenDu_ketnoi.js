01013e01465d924983f00c062bfbb1ffc0e449d0
Hoàng Ngọc Lâm <hoangngoclamlc@gmail.com>
2026-06-23 22:45:28 +0700

    Add files via upload

---
+++ NguyenDu_ketnoi.js
@@ -0,0 +1,23 @@
+/**
+ * TỆP KẾT NỐI HỆ THỐNG (ketnoi.js)
+ * Vị trí lưu trữ: GitHub (Cùng cấp với tệp index.html)
+ * Chức năng: Cấu hình tập trung thông tin định danh và địa chỉ máy chủ (API).
+ */
+
+const KETNOI = {
+    // 1. Thông tin định danh cơ quan/đơn vị
+    TEN_DINH_DANH: "Nguyễn Du OS",
+    TIEU_DE_TRANG: "Cổng thông tin Quản trị - Trường Tiểu học Nguyễn Du",
+    
+    // 2. Tài nguyên hình ảnh đại diện (Logo & Giao diện)
+    LINK_LOGO: "https://i.ibb.co/8Df1xXsN/Logo-nguyen-du.png",
+    LINK_HINH_NEN: "https://i.ibb.co/hJy3vH7y/hinh-nen-win-11-50-1.jpg",
+    LINK_AVATAR_NAM: "https://lh3.googleusercontent.com/d/13OnSd7t73KO7GP6NB7FjkdpsO6vfH-dp",
+    LINK_AVATAR_NU: "https://lh3.googleusercontent.com/d/1zW6N_UQKGYl_uCOb9jMfvZCHkLVJKNVU",
+    
+    // 3. Đường dẫn máy chủ xử lý dữ liệu (Google Apps Script API)
+    WEB_APP_URL: "https://script.google.com/macros/s/AKfycbxlgzfQI5YQQG6q91spFUG2n3KW5nhyW4Xka5Lp8XZD7pRVACa44F1qeb4bfuH27BhA6g/exec",
+
+    // 4. Mã định danh ứng dụng Google (OAuth 2.0 Client ID)
+    CLIENT_ID: "1097384743947-1jdc5rhhmbu0s9jp5vgt814g4f4id7lu.apps.googleusercontent.com"
+};
