1cde454d2562ff8cef783a6a19b8750f1c90b22e
Hoàng Ngọc Lâm <hoangngoclamlc@gmail.com>
2026-07-04 19:34:11 +0700

    Update ketnoi.js

--- QuanLyHoSo/ketnoi.js
+++ QuanLyHoSo/ketnoi.js
@@ -6,19 +6,19 @@
 
 const KETNOI = {
     // 1. Đường dẫn máy chủ xử lý dữ liệu (Google Apps Script API)
-    WEB_APP_URL: "https://script.google.com/macros/s/AKfycbxJEriNwHp3twwlVk_yqViktUo96XOj71ciMdNsNY8TSuH_76t7gPt48DGnWU34gWVdeQ/exec",
+    WEB_APP_URL: "https://script.google.com/macros/s/AKfycbxENr-5IhHLob_kNnsy9UlfL8V9mC7yFEivfxYDLYe07A62kO_IoFzMJUt6P1yKCmJW2A/exec",
 
     // 2. Mã định danh ứng dụng Google (OAuth 2.0 Client ID)
     CLIENT_ID: "1097384743947-1jdc5rhhmbu0s9jp5vgt814g4f4id7lu.apps.googleusercontent.com",
     // 3. Thông tin định danh cơ quan/đơn vị
-    TEN_DINH_DANH: "Hợp Thành OS",
-    TIEU_DE_TRANG: "Cổng thông tin Quản lý hồ sơ - Trường TH&THCS Hợp Thành",
+    TEN_DINH_DANH: "Nguyễn Du OS",
+    TIEU_DE_TRANG: "Cổng thông tin Quản lý hồ sơ - Trường Tiểu học Nguyễn Du",
     
     // Bổ sung liên kết YouTube Hướng dẫn
     LINK_YOUTUBE: "https://www.youtube.com/channel/UCdfiTWwv78VITvzlgxnv02Q",
     
     // 4. Tài nguyên hình ảnh đại diện (Logo & Giao diện)
-    LINK_LOGO: "https://i.ibb.co/6R8Y9DNq/logo-l-a-ch-n6.png",
+    LINK_LOGO: "https://i.ibb.co/8Df1xXsN/Logo-nguyen-du.png",
     LINK_HINH_NEN: "https://bigpicturesb.org/wp-content/uploads/2024/10/anh-nen-win-10-51-scaled.jpeg",
     LINK_AVATAR_NAM: "https://lh3.googleusercontent.com/d/13OnSd7t73KO7GP6NB7FjkdpsO6vfH-dp",
     LINK_AVATAR_NU: "https://lh3.googleusercontent.com/d/1zW6N_UQKGYl_uCOb9jMfvZCHkLVJKNVU"
